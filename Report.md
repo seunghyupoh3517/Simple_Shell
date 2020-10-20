d2f8cce55919c74ac6b70c9006206ad51d8d2c84
Seunghyup Alex Oh <seunghyupoh3517@gmail.com>
2020-10-19 19:51:51 -0700

    Add files via upload

---
+++ Report.md
@@ -0,0 +1,57 @@
+# Simple Shell Report
+
+### Read & Parse Commands
+Read the commands from Standard input into _char* cmd_ and return the string.
+- We dynamically allocate the memory of max buffer size, _CMDLINE MAX_ to _char* cmd_.
+- Return the C string with _'\0'_ to notify the library to where the string ends.
+
+With the returned C string, we parse the string into the struct object, _struct commands_ with double pointer variable, _char **arguments_ in it.
+- We dynmaically allocate the memory of max token size,  _TOKEN MAX_ to _** token_.
+- We parse the string using _*strtok_ with delimiters, _\t\r\n_ to capture and remove white spaces between each argument in command string.
+- While parsing throughout the string, we count the size of arugments, numbers of argument in the commands, and direct the pointer to the size to counted numbers.
+- We assign parsed command string whihc is tokenized into each argument to data structure's _char ** arguments_ and return it.
+
+### Executing Commands
+Unless the command arugment indicates _output redirection_ or _piping arguments_, the commands gets to executed through regualr _int execute cmd(char ** args)_. The execution commands models the behavior of _system()_ in low level to use for implementing realistic shell and return the return value, the corresponding exit value.
+- We first differentiate if the arguments are one of the built in commands; then, if so, we detect which built in command is called by using _built cmd_, tagging integer value and execute the _execute builtin commands(args, built cmd)_ 
+- With the rest of the commands, we simulate _fork()+exec()+wait()_ method: create a  child process, execute child process the speicified command while the parent process waits until child process ends executing and the parent process collects the exit value.
+- _execvp_ is used to execute process in order to execute the complete paths of the commands, implicitly search accrdoing to _$PATH_ enviromnet variable.
+- We finally return the collected exit value.
+
+### Pipeline Commands
+Once the command string is parsed and the string is detected as cotaining the _pipe dilimiter_, "|", we count numbers of pipe in order to check how many arguments is input-ed. Then we pass the parsed string, numbers of pipe delimiter to _execute pipe_.
+- 
+
+
+
+### Built in Commands
+Once one of the built in commands is detected then we call *execute_builtin_commands* with the appropriate enum element. Inside the *execute_builtin_commands* function, we find the command that was passed and move to another function to execute it.
+1- *execute_pwd* function for pwd command:
+In this function, we do a system call to get the current directory path using *getcwd*. If we get the current directory we print it, otherwise we print an error message saying that it failed.
+2- *execute_cd* function for cd command, we pass the path that we want to change into:
+In this function, we do a system call to change the directory we are at to whatever *path* is. We do this using *chdir*. After *chdir* returns, we check if it succeeded depending on its return value.
+3- *execute_sls* function for the extra feature command sls:
+In this function, we use the data type DIR to save the current directory and we use the struct dirent to get specific information about the directory. Here we used three system calls; *opendir*, *readdir*, *stat*, and *closedir*. First we open the directory and check if it opened correctly, if not we print an error message and return. However, if we succeed in opening it, we continue to read every element in the directory. We use *stat* to get the size of the file and print the files in order. Finally we close the directory.
+
+### Output Redirection
+After we parse the command, we check using *strstr* if > is a substring of the command that was given by the user. If ">" is spotted then we call *output_redirection*. This function recieves two arguments; the parsed command and the position of ">".
+- We allocate memory for an array of strings that will hold the new arguments for the executable or command given without ">" and the file name. 
+- We open the file with apporpriate macros depending if the user asked to append to a file or not. 
+- We save the STDOUT file descriptor to return all file descriptors back to normal after we redirected the output.
+- We use *dup2* to change stdout from outputing to the shell to the file given.
+- We check whether we're redirecting the output of a regular command or not.
+- If we are redirecting the ouput of a normal executable we find the path to the executable using *realpath*. We construct the argument that will be sent to exec and we then call *execute_cmd* to execute the executable.
+- If we are redirecting the output of a regular command, we remove the output redirection symbol and the file name and call *execute_cmd*.
+- Finally, we free the memory, return stdout to its original file descriptor and close all the open file descriptors.
+
+
+  
+### References
+1- https://pubs.opengroup.org/onlinepubs/009695399/functions/ was used to understand system calls more for *execute_builtin_commands*.
+2- https://pubs.opengroup.org/onlinepubs/000095399/functions/realpath.html and https://www.geeksforgeeks.org/dup-dup2-linux-system-call/ helped with output redirection. 
+3- https://stackoverflow.com/questions/12784766/check-substring-exists-in-a-string-in-c for strings.
+4- http://www.tutorialdost.com/C-Programming-Tutorial/22-C-Structure-Function.aspx#:~:text=Structure%20can%20be%20passed%20to,value%20or%20by%20references%20%2F%20addresses to grasp better understanding of Strucuture object & Functions in C
+5- https://www.tutorialspoint.com/c_standard_library/c_function_strtok.htm was used to understand application of strtok(), tokenizing the string with the delimiters
+6- https://www.geeksforgeeks.org/making-linux-shell-c/ was used to understand how piping works
+
+###### By *Gharam Alsaedi* & *Seunghyup Alex Oh*
--- sshell.c
+++ sshell.c
@@ -8,10 +8,24 @@
 #include <fcntl.h>
 #include <dirent.h>
 #define CMDLINE_MAX 512
-#define PATH_MAX 4096 // Linux defines PATH_MAX 4096 bytes
+#define PATH_MAX 4096 
 #define TOKEN_MAX 32
 
-enum { exit_cmd = 0, pwd_cmd = 1, cd_cmd = 2, sls_cmd = 3};
+enum {exit_cmd = 0, pwd_cmd = 1, cd_cmd = 2, sls_cmd = 3};
+//enum {READ = 0, WRITE = 1};
+
+int execute_pwd() {
+    char cwd[PATH_MAX];
+    int ret_val = 0;
+    if(getcwd(cwd, sizeof(cwd)) != NULL) {
+        fprintf(stdout, "%s\n", cwd);
+        }
+    else {
+        ret_val = 1;
+        perror("Error: pwd failed");
+    }
+    return ret_val;   
+}
 
 int execute_cd(char * path) {
     int ret_val;
@@ -22,6 +36,7 @@ int execute_cd(char * path) {
     }
     return ret_val;
 }
+
 int execute_sls() {
     int ret_val = 0;
     DIR *dirp;
@@ -37,18 +52,6 @@ int execute_sls() {
     }
     return ret_val;
 }
-int execute_pwd() {
-    char cwd[PATH_MAX];
-    int ret_val = 0;
-    if(getcwd(cwd, sizeof(cwd)) != NULL) {
-        fprintf(stdout, "%s\n", cwd);
-        }
-    else {
-        ret_val = 1;
-        perror("Error: pwd failed");
-    }
-    return ret_val;   
-}
 
 int execute_builtin_commands(char** args, int cmd_num){
     int ret_val;
@@ -60,7 +63,7 @@ int execute_builtin_commands(char** args, int cmd_num){
             ret_val = execute_cd(args[1]);
             break;
         case 3:
-           ret_val = execute_sls();
+            ret_val = execute_sls();
             break;
         default:
             ret_val = 0;
@@ -68,109 +71,80 @@ int execute_builtin_commands(char** args, int cmd_num){
     }
     return ret_val;
 }
-char *read_cmd(void){
-        // memory allocation
-        char *cmd = malloc(sizeof(char) * CMDLINE_MAX);
-        //int cmd_len = strlen(cmd);
-        
-        
-        /* Print command line if stdin is not provided by terminal */
-        if (!isatty(STDIN_FILENO)) { // isatty(fd) test whether a fd is open fd refers to a terminal
-                printf("%s", cmd);
-                fflush(stdout);
-        }
 
+char *read_cmd(void){
+        char *cmd = malloc(sizeof(char) * CMDLINE_MAX);
         if(!cmd){
-                //fprintf(stderr, "ERROR: malloc");
-                perror("ERROR: malloc");
+                perror("ERROR: Malloc");
                 exit(EXIT_FAILURE);
         }
 
-        // reading the cmd line 
-        // fgets(cmd, CMDLINE_MAX, stdin);
         int offset;
         do{
                 offset = read(STDIN_FILENO, cmd, CMDLINE_MAX);
-                //printf("%s%d\n", cmd, offset);
-                if(offset == -1) {// at the end of the txt
-                        //fprintf(stderr, "ERROR: read cmd_line");
-                        perror("ERROR: read cmd_line");
-                        exit(EXIT_FAILURE);
+                if (!isatty(STDIN_FILENO)) { 
+                        printf("%s", cmd);
+                        fflush(stdout);
                 }
-                // reset cursor pointer to NULL
+                if(offset == -1) 
+                        exit(EXIT_FAILURE);
+        
                 cmd[offset] = '\0';
-                
                 return cmd;
         }while(1);
 }
 
-// parsing the cmd_line using string tokenization 
-// ---------------------------------------------- USE struct object 
-
-/*
-#define delimiter = " \t\r\n";
-*/
-struct parsed_token{
-        char **tokens;
-        char *tok;
-        //int length;
-        //int pointer;
+struct commands{
+        char **arguments;
+        //char *argument;
+        // i.e. char *argument1 = {date '\0'} char 
+        // *argument2 = {tr 2 1 '\0'}
+        // '\0' orginally |
+        // char **arguments = {argument1, argument2} = {{{date '\0'}, {tr 2 1 '\0'} }
+        // any difference between char **arguments = {date '\0' tr 2 1 '\0' } ?
 };
-// ---------------------
+//void divide_argument(struct commands *obj){
+//      char **i_arugments = obj->arguments;
+//      char *argument1
+//      char *argument2 
+//}
 
-void assign_token(struct parsed_token *p_tokens, char *p_tok){
-        p_tokens->tok=p_tok;
-}
-
-char **parse_cmd(char *cmd, int* size){ //struct parsed_token *p_tokens, 
-        // memory allocation
+char **parse_cmd(struct commands *obj, char *cmd, int* size){
         char *s_token;
         char **token = malloc(sizeof(char*) * TOKEN_MAX);
-        int size_tokens = 0;
         if(!token){
-                //fprintf(stderr, "ERROR: malloc");
-                perror("ERROR: malloc");
+                perror("ERROR: Malloc");
                 exit(EXIT_FAILURE);
         }
 
-        // ref: https://www.tutorialspoint.com/c_standard_library/c_function_strtok.htm
         int pointer = 0;
-        //printf("debug1, %s\n",cmd);
-        s_token = strtok(cmd, " \t\r\n"); // common delimiter - escape character
-        //struct parsed_token s_tokens = *p_tokens;
+        int size_tokens = 0;
+        s_token = strtok(cmd, " \t\r\n"); 
         while(s_token != NULL){
-                // ---------------------
-                
-                //printf("debug, %s\n", s_token);
-                //assign_token(&s_tokens,s_token);
-                //printf("debug2, %s\n",p_tokens->tok);
-                
-                // ---------------------
-               
-
                 token[pointer] = s_token;
                 pointer++;
                 s_token = strtok(NULL, " \t\r\n");
                 size_tokens++;
         }
         token[pointer] = NULL;
+
+        obj->arguments = token;
         *size = size_tokens;
-        return token;
+
+        return (obj->arguments);
 }
 
 int execute_cmd(char **args){
     int built_cmd = -1;
         if(args[0] == NULL)
                 return EXIT_FAILURE;
-        // ---------------------------- Check whether it is Builtin command or not
-        /* Builtin command */
-        // execute built in - return (return status)
-        if (!strcmp(args[0], "exit")) {  // string compare, equal == 0
+
+        if (!strcmp(args[0], "exit")) {  
                 fprintf(stderr, "Bye...\n");
                 free(args[0]);
                 fprintf(stderr, "+ completed 'exit' [%d]\n",
                         0);
-                return EXIT_FAILURE;
+                return -1;
         }
         if(!strcmp(args[0], "pwd")) {
             built_cmd = pwd_cmd;
@@ -183,39 +157,89 @@ int execute_cmd(char **args){
         }
         if(built_cmd != -1) {
             return execute_builtin_commands(args,built_cmd);
-            built_cmd = -1;
+            built_cmd = -1; //?
         }
-
-       
-
-
 
         pid_t pid;
         pid = fork();
-        if(pid==0){ // child process
-                if(execvp(args[0], args)==-1)
+        if(pid==0){ 
+                if(execvp(args[0], args) == -1)
                         return EXIT_FAILURE;
                 return EXIT_SUCCESS;
-        } else if(pid > 0){ // parent
+        } else if(pid > 0){ 
                 int status;
                 waitpid(pid, &status, 0);
                 if(WEXITSTATUS(status) == -1)
                         return EXIT_FAILURE;
                 return EXIT_SUCCESS;
-        } else{ // forking error
+        } else{ 
                 perror("fork");
                 exit(1);
         }
 
         return EXIT_SUCCESS;
 }
+/*
+int helper_pipe(int i_fd, int o_fd, struct commands *obj){
+        pid_t pid = 0;
+        if((pid == fork()) == 0){
+                if(i_fd != 0){
+                        dup2(i_fd, 0);
+                        close(i_fd);
+                }
 
-// pipe three arguments maximum
-// ref: https://www.geeksforgeeks.org/making-linux-shell-c/
-// int execute_pipe(char* )
-// void execute_pipe(char **args1, char**args2){
-//
-//} (A | B ) = (A' | C)
+                if(o_fd != 1){
+                        dup2(o_fd, 1);
+                        close(o_fd);
+                }
+                return execvp(obj->arguments[0], (char * const *) obj->arguments);
+        }
+        return pid;
+}
+
+int execute_pipe(struct commands *obj, int pipe_count){
+        int fd[2];
+        int f_fd = 0;
+        int i = 0;
+        for (i = 0; i < pipe_count; i++){
+                pipe(fd);
+                helper_pipe(f_fd, fd[1], obj + i);
+                close(fd[1]);
+                f_fd = fd[0];
+        }
+        if(f_fd != 0)
+                dup2(f_fd, 0);
+        return execvp(obj->arguments[i], (char * const *)obj->arguments[i]);
+}
+*/
+
+int execute_pipe(struct commands *obj){
+        int fd[2];
+        pid_t pid;
+        int fd_in = 0;
+        int retval = 0;
+        while (*obj->arguments != NULL) // to take multiple arguments
+        {
+                pipe(fd);
+                if ((pid = fork()) == -1){
+                        exit(EXIT_FAILURE);
+                } else if (pid == 0){
+                        dup2(fd_in, 0); //change the input according to the old one 
+                        if (*(obj->arguments + 1) != NULL)
+                                dup2(fd[1], 1); // change the output to the std output
+                        close(fd[0]);
+                        retval  = execvp((obj->arguments)[0], (char * const *) obj->arguments); // might need to recalle execute_cmd
+                        exit(EXIT_FAILURE);
+                } else{
+                        wait(NULL); // until child has terminated
+                        close(fd[1]);
+                        fd_in = fd[0]; //input data for the next command
+                        obj->arguments++;
+        }
+    }
+        return retval;
+}
+
 
 int output_redirection(char** args, int cmd_pos) {
         char* path = malloc(sizeof(char) * PATH_MAX);
@@ -274,63 +298,61 @@ int output_redirection(char** args, int cmd_pos) {
 }
 int main(void) 
 {
-        //char cmd[CMDLINE_MAX]; 
-        //struct parsed_token *p_tokens = NULL;
         char *cmd; 
         char **token;
+        struct commands command;
+        
         while (1) {
-                //char *nl;
                 int retval;
-                int output_red = 0;
                 int size;
+                int output_red = 0;
+                int pipe_count = 0;
 
-
-                /* Print prompt */
                 printf("sshell$ ");
                 fflush(stdout);
 
-                /* Get command line */
-                //fgets(cmd, CMDLINE_MAX, stdin); // reads a line from stdin stream stores into cmd as string
                 cmd = read_cmd();
-
-                /* Print command line if stdin is not provided by terminal */
-                //if (!isatty(STDIN_FILENO)) { // isatty(fd) test whether a fd is open fd refers to a terminal
-                //        printf("%s", cmd);
-                //        fflush(stdout);
-                //}
-
-                /* Remove trailing newline from command line */
-                //nl = strchr(cmd, '\n'); // returns pointer to first occurrnece of char,\n
-                //if (nl)
-                //        *nl = '\0'; // termination of a char string
-
-                        
-                /* Regular command */
-                //retval = system(cmd);
-                
-
-
-                token = parse_cmd(cmd,&size); //p_tokens
+                token = parse_cmd(&command, cmd, &size);
+                //printf("debug\n");
                 int cmd_pos;
-                for( cmd_pos = 0; cmd_pos < size; cmd_pos++) {
+                char *pipe_dilimiter = "|";
+                for(cmd_pos = 0; cmd_pos < size; cmd_pos++){
+                        if(!strcmp(token[cmd_pos],pipe_dilimiter)){
+                                pipe_count++;
+                                *command.arguments[cmd_pos] = '\0'; 
+                        }
+                } 
+                //printf("debug\n");
+                //int i;
+                //for(i=0;i<size;i++)
+                //        printf("%s\n", command.arguments[i]);
+                
+                if(pipe_count)
+                        retval = execute_pipe(&command);
+                else
+                        retval = execute_cmd(token);
+                
+                // Will have to think about the input with both output direction and pipe - maybe, put this in execute_cmd()
+                // jporquet@pc10:~/ $ echo -e "echo Hello\nexit\n" | ./sshell >& your_output
+                cmd_pos = 0;
+                for(cmd_pos = 0; cmd_pos < size; cmd_pos++) {
                         if(strstr(token[cmd_pos],">") != NULL) {
                                 output_red = 1;
                                 break;
                         }
                 }
-                
                 if(output_red == 1)
                         retval = output_redirection(token,cmd_pos);
-                else
-                        retval = execute_cmd(token); // system(cmd);
-
+//              else
+//                      retval = execute_cmd(token);
+                
                 if(retval == 0)
-                        fprintf(stderr, "+ completed '%s' [%d]\n",
-                        cmd, retval);
+                        fprintf(stderr, "+ completed '%s' [%d]\n", // will need to print out entire cmd
+                        &cmd[0], retval);
+                else if(retval == -1)
+                        break;
                 else
-                        break; 
-                // ------------------------------------
-                // Error Handling - display next sshell
+                        perror("Error: ");
         }
 
         return EXIT_SUCCESS;
